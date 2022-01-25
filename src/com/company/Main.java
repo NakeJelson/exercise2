25aa8873d3d8797d43bc587f25564b13678657a5
NakeJelson <nakejelson78@gmail.com>
2022-01-25 14:20:23 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] weekDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+	int[] temperatures= { 45, 29, 10, 22, 41, 28, 33};
+	int[] precipitationChance = { 95, 60, 25, 5, 0, 75, 90 };
+
+        for (int i = 0; i < weekDays.length ; i++) {
+            if(temperatures[i]<=32&&precipitationChance[i]>50)
+                System.out.println(weekDays[i]);
+            else
+                continue;
+        }
+
+        }
+}
